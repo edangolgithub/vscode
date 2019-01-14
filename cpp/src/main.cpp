f9565b0ffe4533d22faf531189508047ab15a4da
Niraj <Niraj@DESKTOP-5JLFP0L>
2019-01-14 15:07:46 +0545

    rtt

---
+++ cpp/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Easy C++ project!" << std::endl;
+
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::cout<<i;
+    }
+    getchar();
+}
