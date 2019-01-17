853a7bf23f5a0ce132a01f608c6cc8e2caf041c8
Niraj <Niraj@DESKTOP-5JLFP0L>
2019-01-17 14:48:09 +0545

    ert

---
+++ php/operators.php
@@ -0,0 +1,17 @@
+<?php
+//arithematic + - * / %
+//relational < > == >= <= !=
+//logical && || !
+$a=1;  //linus
+$b=2;  // key
+$c=$a^$b; //generated password // 3
+
+echo $c;
+
+echo 2   ^  3;
+
+$x="hello";
+$x.="hi";// $x=$x."hi";
+
+echo $x;
+?>
---
+++ php/switch.php
@@ -0,0 +1,26 @@
+<?php
+$i=1;
+if($i==40)
+{
+    echo 40;
+}
+else if($i==50)
+{
+    echo 50;
+}
+else{
+    echo 90;
+}
+switch($i)  
+{
+    case 40:
+    echo 40;
+break;
+case 50:
+echo 50;
+break;
+default:
+echo 90;
+}
+
+?>
