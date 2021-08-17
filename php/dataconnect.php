81add2763af52d423f17225e0517ee8bad625f15
edangolgithub <dangolevn@gmail.com>
2021-08-17 07:35:08 +0000

    jkhu

--- php/dataconnect.php
+++ php/dataconnect.php
@@ -2,7 +2,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "mysqlpassword";
 $dbname = "school";
 
 // Create connection
---
+++ php/welcome.php
@@ -0,0 +1,7 @@
+<?php
+
+echo $_REQUEST["name"];
+echo "<br />";
+echo $_REQUEST["email"];
+
+?>
